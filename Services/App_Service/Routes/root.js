0f1181d6171c2be3259b2675531f3f45cb5e17f8
Dalia Raafat <daliaraafat03@gmail.com>
2025-01-29 13:08:55 +0200

    layout

--- Services/App_Service/Routes/root.js
+++ Services/App_Service/Routes/root.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("mele");
+  res.render("mele", (layout = True));
 });
 
 router.get("/signup", (req, res) => {
