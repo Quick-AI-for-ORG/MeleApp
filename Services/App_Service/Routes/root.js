41243cb37d73ab5cffc8ca5c3ef831fdd730e993
Dalia Raafat <daliaraafat03@gmail.com>
2025-01-29 11:26:02 +0200

    very initial draft for mele

--- Services/App_Service/Routes/root.js
+++ Services/App_Service/Routes/root.js
@@ -1,17 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index');
+router.get("/", (req, res) => {
+  res.render("mele");
 });
 
-router.get('/signup', (req, res) => {
-  res.render('signup');
+router.get("/signup", (req, res) => {
+  res.render("signup");
 });
 
-router.get('/login', (req, res) => {
-  res.render('login');
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
-
-module.exports = router; 
+module.exports = router;
