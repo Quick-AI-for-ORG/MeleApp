4b3481e1b93ba35ade3f017e4b0832c3f741239d
Adam Gahin <adamlo2ay@gmail.com>
2025-01-29 16:54:59 +0200

    added upgrade schema

---
+++ Services/App_Service/Models/upgrade.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const product = require('./product');
+const upgradeSchema = new mongoose.Schema({
+    userRef: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    productRef: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true,
+    },
+}, { timestamps: true });
