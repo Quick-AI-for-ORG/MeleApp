94f80898270e690add89b854afbcd510ff784876
Dalia Raafat <daliaraafat03@gmail.com>
2025-01-29 13:53:03 +0200

    layout modifications

--- Services/App_Service/Routes/root.js
+++ Services/App_Service/Routes/root.js
@@ -2,15 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("mele", {user : req.session.user === undefined ? "" : req.session.user});
+  res.render("mele", {
+    user: req.session.user === undefined ? "" : req.session.user,
+  });
 });
 
 router.get("/signup", (req, res) => {
-  res.render("signup", {layout:false});
+  res.render("signup", { layout: false });
 });
 
 router.get("/login", (req, res) => {
-  res.render("login", {layout:false});
+  res.render("login", { layout: false });
 });
 
 module.exports = router;
