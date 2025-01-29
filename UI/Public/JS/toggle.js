cb3eac0992d9f6f970973bdc23af92036dade9bd
moshir-ashraf <moshir2107190@miuegypt.edu.eg>
2025-01-29 16:32:10 +0200

    toggle effect

--- UI/Public/JS/toggle.js
+++ UI/Public/JS/toggle.js
@@ -1,12 +1,21 @@
-let sin = document.getElementById("in");
-let sup = document.getElementById("up");
+let sin = document.getElementById("on");
+let sup = document.getElementById("off");
+sin.id = 'on'
+sup.addEventListener("mouseout", function () {
+  sin.id= 'off'
+  sup.id= 'on';
+});
+
+sin.addEventListener("mouseout", function () {
+  sup.id = 'off';
+  sin.id = 'on';
+});
+
 
 sup.addEventListener("mouseover", function () {
-  sin.classList.remove("btn-dark");
-  sup.classList.add("btn-dark");
+  sin.id= 'off'
 });
 
 sin.addEventListener("mouseover", function () {
-  sup.classList.remove("btn-dark");
-  sin.classList.add("btn-dark");
+  sup.id = 'off';
 });
