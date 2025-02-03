59335a262bd1ed33b1ece87db8ecebffef87d445
Navanivous <Khaled2003yehia@gmail.com>
2025-02-03 12:13:17 +0200

    ctrlPages calling

--- Services/App_Service/Routes/keeper.js
+++ Services/App_Service/Routes/keeper.js
@@ -4,48 +4,16 @@ const mongoose = require("mongoose");
 const ctrlPages = require("../Controllers/ctrlPages");
 
 
-router.get("/", (req, res) => {
-  req.session.message = "Please login to access this page";
-  res.redirect("/keeper/login");
-});
+router.get("/",ctrlPages._KEEPER.noLogin);
+
 router.get("/signup", ctrlPages._KEEPER.signup);
 
 router.get("/login", ctrlPages._KEEPER.login);
 
-router.get("/dashboard", (req, res) => {
-  res.render("beekeeper", {
-    layout: false,
-    message: req.body.message === undefined ? null : req.body.message,
-  });
-});
+router.get("/dashboard",ctrlPages._KEEPER.dashboard);
 
-router.get("/upgrade", async (req, res) => {
-  try {
-    const meleDB = mongoose.connection.useDb("meleDB");
+router.get("/upgrade", ctrlPages._KEEPER.upgrade);
 
-    const collections = await meleDB.db.listCollections().toArray();
-
-    const kits = await meleDB.db.collection("products").find({}).toArray();
-
-    res.render("upgrade", {
-      user: req.session.user || "",
-      layout: false,
-      kits: kits,
-      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
-    });
-  } catch (error) {
-    console.error("Database Error:", error);
-    res.render("upgrade", {
-      user: req.session.user || "",
-      layout: false,
-      kits: [],
-    });
-  }
-});
-
-router.post("/upgrade", (req, res) => {
-  const upgradeData = req.body;
-  res.redirect("/keeper/dashboard");
-});
+router.post("/upgrade", ctrlPages._KEEPER.postUpgrade);
 
 module.exports = router;
