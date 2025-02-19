4bc36b5628f5025077f39820b3219ba218d1f682
Dalia Raafat <daliaraafat03@gmail.com>
2025-02-19 17:00:04 +0200

    hive upgrades

--- Services/App_Service/Routes/keeper.js
+++ Services/App_Service/Routes/keeper.js
@@ -3,13 +3,12 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const ctrlPages = require("../Controllers/ctrlPages");
 const ctrlUser = require("../Controllers/ctrlUser");
+const ctrlProduct = require("../Controllers/ctrlProduct");
 
 router.get("/", (req, res) => {
   if (!req.session.user) ctrlPages._PUBLIC.noLogin(req, res);
   else res.redirect("/keeper/dashboard");
 });
-
-
 
 router.get("/signup", ctrlPages._KEEPER.signup);
 router.get("/login", ctrlPages._KEEPER.login);
@@ -22,6 +21,14 @@ router.post("/register", ctrlUser.register);
 router.post("/login", ctrlUser.login);
 router.post("/profile/update", ctrlUser.updateUser);
 
+router.get("/getProducts", async (req, res) => {
+  try {
+    const result = await ctrlProduct.getProducts(req, res);
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
 
 router.get("*", (req, res) => {
   res.redirect("/keeper");
