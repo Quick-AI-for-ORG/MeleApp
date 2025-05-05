b004b01448251709475ae7e7191c9fe1384fde55
Dalia Raafat <daliaraafat03@gmail.com>
2025-05-05 20:34:48 +0300

    fixed

--- Services/App_Service/Controllers/ctrlApiary.js
+++ Services/App_Service/Controllers/ctrlApiary.js
@@ -50,7 +50,12 @@ const updateApiary = async (req, res) => {
 const getApiary = async (req, res) => {
     try {
         const result = await Apiary.get(req.body._id);
-        return res.json(result.toJSON());
+        if(req.session.isHardware){
+            req.session.isHardware = false;
+            return res.json(result.toJSON())
+        }
+
+        return result.toJSON();
     } catch (error) {
         return res.json(new Result(-1, null, `Error fetching apiary: ${error.message}`).toJSON());
     }
